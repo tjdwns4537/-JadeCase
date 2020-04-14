6d960868c98852ad838b30c10eece55d0c2210cf
tjdwns4537 <tjdwns4537@nate.com>
2020-04-15 00:20:39 +0900

    배열메서드연습

---
+++ 배열메서드연습.js
@@ -0,0 +1,15 @@
+var arr = [
+  {x1: 1, x2: 1},
+  {x1: 2, x2: 2},
+  {x1: 3, x2: 3}
+];
+console.log(arr.x1)
+var arr2 =null;
+arr2 = arr.map(obj=>{
+    return {
+      x1: obj.x1,
+      x2: obj.x2,
+      result : obj.x1*obj.x2
+    };
+})
+console.log(arr2)
